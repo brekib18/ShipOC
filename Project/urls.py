66b4457276e18f6fc46422c4142f1fb2a3e14754
Katrín Mist Kristinsdóttir <katrinmistkristinsdottir@Katrins-MacBook-Pro-3.local>
2021-05-05 10:12:30 +0000

    katrin

--- Project/urls.py
+++ Project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('morgunkorn/', include('cereal.urls')),
+    path('fylgihlutir/', include('accessories.urls')),
+    path('föt/', include('merch.urls')),
+    path('bækur/', include('books.urls'))
 ]
