3b8ae5ff56db1b0958e8b3a77ecd52548088b2be
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-05 12:56:03 +0000

    setja klasa a cereal

--- Project/urls.py
+++ Project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include('cereal.urls')),
     path('admin/', admin.site.urls),
     path('morgunkorn/', include('cereal.urls')),
     path('fylgihlutir/', include('accessories.urls')),
