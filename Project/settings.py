0178e4acf58d76474aa48ad7ee2a99ce2385c399
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-06 12:54:42 +0000

    google cloud paelingar

--- Project/settings.py
+++ Project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'shipOC-db',
+        'USER':'shipOC-db-user',
+        'PASSWORD':'ABC12345',
+        'HOST':'34.123.129.175',
+        'PORT':'5432'
     }
 }
 
--- cereal/models.py
+++ cereal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class CerealCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Cereal(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255,blank=True)
+    category = models.ForeignKey(CerealCategory,on_delete=models.CASCADE)
+    price = models.FloatField()
+    on_sale = models.BooleanField()
+
+class CerealImage(models.Model):
+    image = models.CharField(max_length=9999)
+    candy = models.ForeignKey(Cereal, on_delete=models.CASCADE())
 # Create your models here.
