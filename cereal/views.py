4c55b6622e876237dc97802bf47aa794ee9b6d2d
katrink10 <katrinmk18@ru.is>
2021-05-06 12:54:15 +0000

    data base

--- cereal/views.py
+++ cereal/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
 
+cereals = [
+    {'name': 'cocoa puffs', 'price': 4.99},
+    {'name': 'musli', 'price': 8.99}
+]
+
 def index(request):
-    return render(request, 'cereal/index.html')
+    return render(request, 'cereal/index.html', context = {'cereals': cereals})
