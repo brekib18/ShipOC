e307f94ccdffd89b3f755975370a8778588f863f
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-07 13:38:02 +0000

    category

--- cereal/models.py
+++ cereal/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from frontpage.models import Category
 class CerealCategory(models.Model):
     name = models.CharField(max_length=255)
 
 class Cereal(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255,blank=True)
-    category = models.ForeignKey(CerealCategory,on_delete=models.CASCADE)
+    cereal_category_id = models.ForeignKey(CerealCategory,on_delete=models.CASCADE)
     price = models.FloatField()
+    on_sale = models.BooleanField()
+    category_id = models.ForeignKey(Category,on_delete=models.CASCADE)
 
 class CerealImage(models.Model):
     image = models.CharField(max_length=9999)
--- frontpage/models.py
+++ frontpage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-class category(models.Model):
+
+class Category(models.Model):
     name = models.CharField(max_length=255)
 
 class CategoryImage(models.Model):
