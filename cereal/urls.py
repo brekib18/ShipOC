b9edbeadd6c94c5b54e2e3d4763797991a2771f7
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-10 10:06:38 +0000

    morgunkorns css

--- cereal/urls.py
+++ cereal/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="cereal-index"),
+    path('<int:id>',views.get_cereal_by_id,name="cereal-details")
 ]
--- cereal/views.py
+++ cereal/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from cereal.models import Cereal
+from django.shortcuts import render
+from cereal.models import Cereal
+from django.shortcuts import get_object_or_404
+
+
 # Create your views here.
+
 
 
 
@@ -8,3 +14,8 @@ from cereal.models import Cereal
 def index(request):
     context = {'cereals': Cereal.objects.all().order_by('name')}
     return render(request, 'cereal/index.html', context)
+
+def get_cereal_by_id(request,id):
+    return render(request,'cereal/cereal_details.html',{
+        'cereal': get_object_or_404(Cereal, pk=id)
+    })
