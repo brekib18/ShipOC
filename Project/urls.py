b0a00c145684d00170820fed52766a2097520e45
brekib18 <brekib18@ru.is>
2021-05-10 11:41:06 +0000

    info bætt vid

--- Project/urls.py
+++ Project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('morgunkorn/', include('cereal.urls')),
     path('fylgihlutir/', include('accessories.urls')),
     path('fot/', include('clothes.urls')),
-    path('bakur/', include('books.urls'))
+    path('bakur/', include('books.urls')),
+    path('upplysingar/', include('info.urls'))
 ]
