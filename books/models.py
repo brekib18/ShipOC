4815fc886a1ed367aa4a5db3dd11b6358d53827b
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-10 11:59:04 +0000

    models.py

--- books/models.py
+++ books/models.py
@@ -6,7 +6,7 @@ class Books(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255,blank=True)
     category = models.ForeignKey(BooksCategory,on_delete=models.CASCADE)
-    price = models.FloatField()
+    price = models.IntegerField()
 
 class BooksImage(models.Model):
     image = models.CharField(max_length=9999)
