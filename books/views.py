e0f6e59dc49be1faeb1e9332dd16cab9abfdd9dc
katrink10 <katrinmk18@ru.is>
2021-05-11 13:05:32 +0000

    js

--- books/views.py
+++ books/views.py
@@ -7,7 +7,20 @@ def index(request):
     context = {'books': Books.objects.all().order_by("name")}
     return render(request, 'books/index.html',context)
 
+<<<<<<< Updated upstream
 def get_book_by_id(request,id):
     return render(request,'books/books_details.html',{
         'book': get_object_or_404(Books, pk=id)
     })
+=======
+
+def create_book(request):
+    if request.method == 'POST':
+        print(1)
+    else:
+        form = BookCreateForm()
+        # TODO: Instance new BookCreateForm()
+    return render(request, 'book/create_book.html', {
+        'form': form
+    })
+>>>>>>> Stashed changes
---
+++ static/js/cereals.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#search-btn').on('click', function(e){
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        $.ajax( {
+            url: '/morgunkorn?search_filter=' + searchText,
+            type: 'GET',
+            success: function(res){
+                var newHtml = res.data.map(d => {
+                    return <div class="well candy">
+                            <a href="/morgunkorn/${d.id}">
+                                <img class="cereal-img" src="${d.firstImage}" />
+                                <h4>${d.name}</h4>
+                                <p>${d.description}</p>
+                            </a>
+                    </div>
+                });
+                $('.cereals').html(newHtml.join(''));
+                $('search-box').val('');
+            },
+            error: function(xhr, status, error){
+                //TODO: show toastr
+                console.error(error);
+            }
+        })
+    });
+});
