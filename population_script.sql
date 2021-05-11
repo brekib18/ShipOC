d4a0b05c0912f87e4a386f57f6b4c609463a3a6e
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-11 15:43:23 +0000

    breyting a gagnagrunni

--- population_script.sql
+++ population_script.sql
@@ -10,8 +10,8 @@ insert into cereal_cerealcategory(name) values ('Sykurlítið');
 
 <<<<<<< Updated upstream
 -- CEREAL
-insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Cocoa Puffs','Hver elskar ekki Cocoa Puffs? Hrikalega bragðgott.','650',37,29,False);
-insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Cheerios','Eitt allra vinsælasta morgunkorn allra tíma!','420',37,30,False);
+insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Cocoa Puffs','Hver elskar ekki Cocoa Puffs? Hrikalega bragðgott. Cocoa puffs inniheldur sykur, litarefni, hafra, sterkju, rotvarnarefni og egg.','650',37,29,False);
+insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Cheerios','Eitt allra vinsælasta morgunkorn allra tíma! Cheerios innihledur rotvarnarefni, maíssterkju, ýruefni, salt, bindiefni, vítamín, steinefni og hafra.','420',37,30,False);
 insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Weetabix','Hollt, gott og vegan!','400',37,28,False);
 insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Corn Flakes','Morgunkornið sem allir elska.','580',37,28,False);
 insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Barbara’s Shredded Wheat Cereal','100% sykulaust','500',37,30,False);
@@ -20,15 +20,15 @@ insert into cereal_cereal(name, description, price, category_id_id, cereal_categ
 insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Frosted Flakes','Sykurhúðað Corn Flakes.','600',37,29,False);
 insert into cereal_cereal(name, description, price, category_id_id, cereal_category_id_id,on_sale) values ('Crispy Wheat & Raisins','Fullkomin máltið í skál.','440',37,30,False);
 
-insert into cereal_cerealimage(image, cereal_id) values ('https://png2.cleanpng.com/sh/45f62e997d18b00cce9e407a492558cd/L0KzQYm3VsE6N6Ztj5H0aYP2gLBuTfJzbZJwftN8dD3mdcPsgfwuepZqi9c2cz3zhbftk71zbZZ4fZ98LYDocbB8lL1jfaV5RadrMkm3drXtUsJiOZQ9RqsAOEG4SYO4UcUzQWU4TKICOUG3QYq1kP5o/kisspng-breakfast-cereal-reese-s-puffs-reese-s-peanut-butt-5b294fdf22a1c8.9581592115294340791419.png',46);
-insert into cereal_cerealimage(image, cereal_id) values ('https://www.pngitem.com/pimgs/m/546-5465420_transparent-cereal-cheerios-cheerios-cereal-hd-png-download.png',47);
-insert into cereal_cerealimage(image, cereal_id) values ('https://img.favpng.com/2/6/21/breakfast-cereal-weet-bix-weetabix-whole-grain-png-favpng-37tZqytAqtrP1vCWjudM6nHb3.jpg',48);
-insert into cereal_cerealimage(image, cereal_id) values ('http://images.kglobalservices.com/www.kelloggs.com.au/en_au/product/product_449/prod_img-198128_kelloggs-corn-flakes.png',49);
-insert into cereal_cerealimage(image, cereal_id) values ('https://www.barbaras.com/wp-content/uploads/2019/11/Barbaras-Shredded-Wheat-Cereal-Box.png',50);
-insert into cereal_cerealimage(image, cereal_id) values ('https://icon2.cleanpng.com/20180420/lre/kisspng-breakfast-cereal-kellogg-s-corn-pops-cereal-milk-a-corn-pops-5ad9d434358905.5683214115242250762193.jpg',51);
-insert into cereal_cerealimage(image, cereal_id) values ('http://images.kglobalservices.com/www.kashi.com/en_us/product/product_4508544/prod_img-1107114_ks_rtec_h2h_png.png',52);
-insert into cereal_cerealimage(image, cereal_id) values ('https://img.favpng.com/16/25/25/frosted-flakes-breakfast-cereal-corn-flakes-frosting-icing-png-favpng-kBRcA6Bu6esnYrqseVKjNrMZ5.jpg',53);
-insert into cereal_cerealimage(image, cereal_id) values ('http://images.kglobalservices.com/www.kelloggs.ie/en_ie/product/product_253594/prod_img-253850_05053827165808_a1l1_25_uk.png',54);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('https://cereals.generalmills.com/wp-content/uploads/2019/04/cocoa-puffs.png','https://img2.pngio.com/foodcolgate-cocoa-puffs-png-314_240.png',46);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('https://www.pngitem.com/pimgs/m/546-5465420_transparent-cereal-cheerios-cheerios-cereal-hd-png-download.png','https://pngimage.net/wp-content/uploads/2018/05/bowl-of-cheerios-png-2.png',47);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('https://img.favpng.com/2/6/21/breakfast-cereal-weet-bix-weetabix-whole-grain-png-favpng-37tZqytAqtrP1vCWjudM6nHb3.jpg','https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/7/8/0/0/1680087-1-eng-GB/Weetabix-contract-dragged-food-recycler-into-administration_wrbm_large.png',48);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('http://images.kglobalservices.com/www.kelloggs.com.au/en_au/product/product_449/prod_img-198128_kelloggs-corn-flakes.png','https://www.pngarea.com/pngm/54/655757_corn-png-bowl-of-corn-flakes-png-download.png',49);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('https://www.barbaras.com/wp-content/uploads/2019/11/Barbaras-Shredded-Wheat-Cereal-Box.png','https://spy.com/wp-content/uploads/2021/03/Overlay-Recovered-Recovered-19.png',50);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('https://icon2.cleanpng.com/20180420/lre/kisspng-breakfast-cereal-kellogg-s-corn-pops-cereal-milk-a-corn-pops-5ad9d434358905.5683214115242250762193.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSCx_gcwpEhB-kfJ9u4vM4g-GzcUo9bCNkYQ&usqp=CAU',51);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('http://images.kglobalservices.com/www.kashi.com/en_us/product/product_4508544/prod_img-1107114_ks_rtec_h2h_png.png','https://www.kelloggs.com/content/dam/NorthAmerica/kelloggs/en_US/images/BrandDetail/Kashi/Kashi_1880x835_hero-image.jpg',52);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('https://img.favpng.com/16/25/25/frosted-flakes-breakfast-cereal-corn-flakes-frosting-icing-png-favpng-kBRcA6Bu6esnYrqseVKjNrMZ5.jpg','https://www.pngarea.com/pngm/54/655757_corn-png-bowl-of-corn-flakes-png-download.png',53);
+insert into cereal_cerealimage(image1,image2, cereal_id) values ('http://images.kglobalservices.com/www.kelloggs.ie/en_ie/product/product_253594/prod_img-253850_05053827165808_a1l1_25_uk.png','https://upload.wikimedia.org/wikipedia/commons/2/2b/Frosted_Mini_Wheats.PNG',54);
 
 -- BOOKS
 insert into books_books(name, description, price, category_id_id) values ('The Cheerios Play Book','Frábær bók um Cheerios fyrir alla krakka.','350',38);
@@ -56,10 +56,10 @@ insert into clothes_clothesimage(image, clothes_id) values ('https://cdn.shopify
 -- accessories
 
 insert into accessories_accessories(name, description, price, category_id) values ('Cheerios mjólkurkanna','Flott Cheerios mjólkurkanna sem tekur 1L.',2.650,40);
-insert into accessories_accessories(name, description, price, category_id) values ('Cocoa puffs sokkar','Mjúkir og hlýir sokkar með Cocoa puffs logo-inu','2.150',39);
+insert into accessories_accessories(name, description, price, category_id) values ('Cheerios bolli','Glæsilegur Cheerios plastbolli. Tilvalinn til að borða Cheerios úr.','2.150',39);
 insert into accessories_accessories(name, description, price, category_id) values ('Frosted flakes sokkar','Frábærir og góðir sokkar','1.999',39);
 
 
 insert into accessories_accessoriesimage(image, clothes_id) values ('https://i.ebayimg.com/images/g/dEMAAOSwBxBc1uX1/s-l400.jpg',1);
-insert into accessories_accessoriesimage(image, clothes_id) values ('https://images-na.ssl-images-amazon.com/images/I/61-lBAxumAL._UX569_.jpg',2);
+insert into accessories_accessoriesimage(image, clothes_id) values ('https://i.pinimg.com/originals/c1/78/d5/c178d5f54976844b156c6fc4d71d6ff9.jpg',2);
 insert into accessories_accessoriesimage(image, clothes_id) values ('https://cdn.shopify.com/s/files/1/0317/1509/products/FF_BACK.jpg',3);
