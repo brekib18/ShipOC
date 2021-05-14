495ddadc85455f578e4865fb298527a9a587aa92
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-14 11:13:49 +0000

    super

--- static/js/chat-btn.js
+++ static/js/chat-btn.js
@@ -8,8 +8,6 @@ $(document).ready(function(){
         newchat.className ='chat_response'
         newchat.textContent = input_val
         chat.appendChild(newchat)
-
-
     })})
 
 function myClick(){
