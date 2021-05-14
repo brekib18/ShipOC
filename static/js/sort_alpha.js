41c7db814e4c38ae6f957c943a8bd3938b398947
Malfridur Anna Eiriksdottir <malfridur18@ru.is>
2021-05-14 19:24:23 +0000

    a

---
+++ static/js/sort_alpha.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.sort_button').on('click', function (e) {
+        console.log(this.value)
+        e.preventDefault();
+        $.ajax( {
+            url: '/cereals?filter='+this.value,
+            type: 'GET',
+            success: function(res){
+
+                var newHtml = res.data.map(d => {
+                    return `<div class="well candy">
+                            <a href="/cereals/${d.id}">
+                                <img class="product-img" src="${d.firstImage}" />
+                                <h4>${d.name}</h4>
+                                <p>${d.price} kr.</p>
+                            </a>
+                    </div>`
+                });
+                $('.cereals').html(newHtml.join(''));
+                $('search-box').val('');
+            },
+            error: function(xhr, status, error){
+                //TODO: show toastr
+                console.error(error);
+            }
+            });
+        });})
